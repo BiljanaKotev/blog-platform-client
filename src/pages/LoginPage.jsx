5c7f6547ca16113caae55fef7e0f3de5cdd24ddf
Biljana Kotevska <99471338+BiljanaKotev@users.noreply.github.com>
2023-10-29 06:29:38 +0100

    Update LoginPage.jsx
    updated spinner style

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -9,12 +9,6 @@ import ClipLoader from 'react-spinners/ClipLoader';
 
 const API_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5005';
 
-const override = {
-  display: 'block',
-  margin: '0 auto',
-  borderColor: 'red',
-};
-
 function LoginPage() {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState('');
@@ -55,31 +49,36 @@ function LoginPage() {
 
   return (
     <main>
-      {loading && <ClipLoader color="#000" loading={loading} css={override} size={50} aria-label="Loading Spinner" />}
-      <form className="auth-form" onSubmit={handleLoginSubmit}>
-        <h1>Login</h1>
+      {loading ? (
+        <ClipLoader color="#0d86d3" loading={loading} size={50} aria-label="Loading Spinner" />
+      ) : (
+        <>
+          <form className="auth-form" onSubmit={handleLoginSubmit}>
+            <h1>Login</h1>
 
-        <div className="email-container">
-          <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" placeholder="ironhacker@gmail.com" value={email} onChange={handleEmail} />
-        </div>
+            <div className="email-container">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" id="email" placeholder="ironhacker@gmail.com" value={email} onChange={handleEmail} />
+            </div>
 
-        <div className="password-container">
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" placeholder="******" value={password} onChange={handlePassword} autoComplete="currrent-password" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+            <div className="password-container">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" id="password" placeholder="******" value={password} onChange={handlePassword} autoComplete="currrent-password" />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
 
-      {errorMsg && (
-        <div>
-          <p className="login-error-msg">{errorMsg}</p>
-        </div>
+          {errorMsg && (
+            <div>
+              <p className="login-error-msg">{errorMsg}</p>
+            </div>
+          )}
+          <p>Don't have an account yet?</p>
+          <Link className="homepage-signup-link" to={'/signup'}>
+            Signup
+          </Link>
+        </>
       )}
-      <p>Don't have an account yet?</p>
-      <Link className="homepage-signup-link" to={'/signup'}>
-        Signup
-      </Link>
     </main>
   );
 }
